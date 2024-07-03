0602d542b0709dc93e6977c93c81d86e00a3b488
Mufaddal <mufaddaltopiwala15@gmail.com>
2024-07-03 23:40:12 +0530

    formatting

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const userId = localStorage.getItem("userId") || (localStorage.setItem("userId",
 
 // Function to generate a UUID (Unique User ID)
 function generateUUID() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
         var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
     });
